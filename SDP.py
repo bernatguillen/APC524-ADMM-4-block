126c6e17bd5da2f8d8da98e86277015c8a6e7759
Bernat Guillen <bguillen@gmail.com>
2014-12-05 14:42:25 -0500

    Changed SDP name, add function toConic

--- SDP.py
+++ SDP.py
@@ -8,42 +8,23 @@ class ErrorDim(Exception):
         errstr = "Dimension should be "+repr(self._dim1)+" but is "+repr(self._dim2)
         return errstr
 
-class SDPProblem(object):
+class DNNSDP(object):
 """Defines an SDP problem with:
      minimize Tr(Copt*X)
      s.t. tr(Aeq[k]*X) == beq[k] k = 0,...,M-1
           tr(Ain[k]*X) <= bin[k] k = 0,...,M'-1
           X is SDP
-          Elements of X may or may not be nonnegative"""
-    def __init__(self, Copt=None, Aeq=None, beq=None, Ain=None, bin=None, doubly = False):
+          Elements of X nonnegative"""
+    def __init__(self, Copt=None, Aeq=None, beq=None, Ain=None, bin=None):
         self._n = Copt.shape[0]
         self._Copt = Copt
         self._Aeq = Aeq
         self._beq = beq
         self._Ain = Ain
         self._bin = bin
-        self._doubly = doubly
 #number of columns of Aeq has to be n
-        if self._Aeq.shape[1] != self._n:
-            try:
-                raise ErrorDim(self._n,self._Aeq.shape)
-            except ErrorDim:
-                print 'Error in equality matrix constraint'
-        if self._beq.shape[0] != self._Aeq.shape[0]:
-            try:
-                raise ErrorDim(self._beq.shape[0], self._Aeq.shape[0])
-            except ErrorDim:
-                print 'Error in equality matrix constraint'
-        if self._Ain.shape[1] != self._n:
-            try:
-                raise ErrorDim(self._n, self._Ain.shape[0])
-            except ErrorDim:
-                print 'Error in inequality matrix constraint'
-        if self._bin.shape[0] != self._Ain.shape[0]:
-            try:
-                raise ErrorDim(self._bin.shape[0], self._Ain.shape[0])
-            except ErrorDim:
-                print 'Error in inequality matrix constraint'
-
+"""For now, no checking of dimensions"""
     def toConic(self):
-        
+#For now, no inequality constraints
+
+        ConicP = ConicProgramming(self._Copt.reshape(self._n),self._Aeq.reshape)
