8f8c3d543f39e93d18745ae667175be0c1890b4c
Yuan <laura425@hotmail.com>
2014-12-15 11:22:22 +0800

    Barebone for self-reference

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import input as INP
 import SDP as SDP
 import Interface as ADMM
 
-
 MySDP = DNNSDP(Copt, Aeq, Ain, bin) # get the input of the SDP problem
 MyConic = MySDP.toConic() # change the input to the standard conic problem
 Solution = ADMM() # update the problem with the ADMM solver
