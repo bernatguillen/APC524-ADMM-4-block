393b88f8ab89b53ad183dd1980ee8db87995ca5e
Bernat Guillén <bguillen@gmail.com>
2015-01-08 15:12:34 -0500

    Fixed setup.py
    The package can be installed with pip now

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1dev',
     packages=['admm4block','admm4block.conic','admm4block.sdp'],
     license='GNU GENERAL PUBLIC LICENSE',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
 )
