71e9262e1f38e994ab951507ed895e5a5962f8c6
Bernat Guillén <bguillen@gmail.com>
2015-01-10 17:30:52 -0500

    setup.py updated to standards

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
-from distutils.core import setup
+from setuptools import setup,find_packages
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here,'README.txt'), encoding = 'utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name='admm4block',
+    
     version='0.1dev',
-    packages=['admm4block','admm4block.conic','admm4block.sdp'],
+    description='ADMM4block solver for DNNSDP',
+    long_description = long_description,
+    
+    url = 'https://github.com/bernatguillen/APC524-ADMM-4-block',
+    
+    author = 'Bernat Guillen, Michael Tarczon, Yuan Liu',
     license='GNU GENERAL PUBLIC LICENSE',
-    long_description=open('README.txt').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Mathematics'
+        ],
+    keywords = 'Convex Optimization SDP',
+        
+    packages=find_packages(exclude = ['contrib','docs','tests*']),
+    install_requires=['numpy'],
+
 )
