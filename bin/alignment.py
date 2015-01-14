33dd8e031b7f780f28bf2ed4c41a5e9b17277fea
Bernat Guillén <bguillen@gmail.com>
2015-01-14 00:20:39 -0500

    Moving alignment.py to bin

--- bin/alignment.py
+++ bin/alignment.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import admm4block
-from make_1Dsig import make_1Dsig
 import numpy as np
 import math
 
+def make_1Dsig(time,L,sigma):
+    I = np.random.randn(L,1)
+    I = math.sqrt(L)*I/np.linalg.norm(I)
+    movie = np.zeros((L,time))
+    shifts = np.random.randint(L, size=time)
+    for t in range(0,time):
+        shift = shifts[t]
+        movie[:,t:t+1] = np.roll(I,-shift) + sigma*np.random.randn(L,1)
+    return (movie,shifts)
+    
 L = 2
 time = 2
 noise = 0.0
