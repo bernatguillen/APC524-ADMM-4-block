127d910bd20c2eaeb9ab21aaab534d1bb9e833c5
Michael Tarczon <mtarczon@princeton.edu>
2015-01-15 00:07:06 -0500

    Changed variable names to be more consistent.

--- bin/alignment.m
+++ bin/alignment.m
@@ -1,28 +1,28 @@
-function [G,cvx_status] = alignment(movie)
+function [X,cvx_status] = alignment(movie)
 
 [L,time] = size(movie);
 
-Y = vec(movie) * (vec(movie))';
+Copt = movie(:) * (movie(:))';
 
 idx = @(i,k) (i-1)*L+k;
 
 cvx_begin quiet
     cvx_solver sedumi
-    variable G(L*time,L*time) semidefinite
-    maximize(trace(Y*G))
+    variable X(L*time,L*time) semidefinite
+    maximize(trace(Copt*X))
     subject to
         for i = 1:time
-            G(idx(i,1):idx(i,L),idx(i,1):idx(i,L)) == eye(L);
+            X(idx(i,1):idx(i,L),idx(i,1):idx(i,L)) == eye(L);
         end
         for i = 2:time
             for j = 1:i-1
                 for k = 1:L
-                    sum(G(idx(i,k),idx(j,1):idx(j,L))) == 1;
+                    sum(X(idx(i,k),idx(j,1):idx(j,L))) == 1;
                     for l = 1:(L-1)
-                        G(idx(i,mod(k-1+l-1,L)+1),idx(j,l)) - G(idx(i,mod(k-1+l,L)+1),idx(j,l+1)) == 0;
+                        X(idx(i,mod(k-1+l-1,L)+1),idx(j,l)) - X(idx(i,mod(k-1+l,L)+1),idx(j,l+1)) == 0;
                     end
                     for l = 1:L
-                        G(idx(i,k),idx(j,l)) >= 0;
+                        X(idx(i,k),idx(j,l)) >= 0;
                     end
                 end
             end
