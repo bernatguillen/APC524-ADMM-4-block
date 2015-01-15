f79aa32a892d56cbca16f883e12b1e02b38a09ab
Michael Tarczon <mtarczon@princeton.edu>
2015-01-15 05:59:11 -0500

    Added code to allow user to compare ADMM and CVX (MATLAB) answers for the same SDP.

--- bin/alignment.py
+++ bin/alignment.py
@@ -3,7 +3,8 @@ import admm4block
 import numpy as np
 import math
 import time as timing
-from mlab.releases import latest_release
+import warnings
+from mlabwrap import mlab
 
 def make_1Dsig(time,L,sigma):
     I = np.random.randn(L,1)
@@ -16,14 +17,14 @@ def make_1Dsig(time,L,sigma):
     return (movie,shifts)
 
 # Initial parameters for the multireference alignment SDP problem
-L = 5
-time = 5
-noise = 0.0
+L = 2
+time = 2
+noise = 0.5
 def index(i,k):
     return i + k*L
 (movie,shifts) = make_1Dsig(L,time,noise)
 
-# Solve using our ADMM package
+########################### Solve using our ADMM package ###########################
 # First build Copy, Aeq, and beq matrices
 Copt = np.reshape(movie,(L*time,1),'F')
 Copt = np.multiply(Copt,Copt.T)
@@ -44,7 +45,7 @@ for k in range(0,time-1):
     for l in range(k+1,time):
 	for i in range(1):
             A = np.zeros((L*time,L*time))
-            A[index(i,k),index(0,l):index(L,l)] = 1;
+            A[index(i,k),index(0,l):index(L,l)] = 1
             Aeq.append(A)
             beq.append(1)
         for i in range(0,L):
@@ -54,12 +55,44 @@ for k in range(0,time-1):
                 A[index((i+j+1)%L,k),index(j+1,l)] = -1
                 Aeq.append(A)
                 beq.append(0)
+#For debugging purposes
+#for k in range(0,time-1):
+#	for l in range(k+1,time):
+#		for i in range(0,L):
+#			for j in range(0,L):
+#				A = np.zeros((L*time,L*time))
+#				A[index(i,k),index(j,l)] = 1
+#				A[index(j,l),index(i,k)] = -1
+#				Aeq.append(A)
+#				beq.append(0)
+
 # Conditions for the solver
 sigma = 1
 tau = 1
-tol = 1e-3
+# See http://cvxr.com/cvx/doc/solver.html for more information on CVX precision
+machine_precision = 2.2e-16
+tol_low = machine_precision**0.25       # Call alignment.m with 1 as second argument
+tol_default = machine_precision**0.375  # Call alignment.m with 2 as second argument
+tol_high = machine_precision**0.5       # Call alignment.m with 3 as second argument
 nsteps = 1000
+start_solve = timing.clock()
 mySDP = admm4block.DNNSDP(Copt, Aeq, beq)
-[X,s,z,y,res,_]=mySDP.Solve(sigma, tau, tol, nsteps)
+[X,s,z,y,res,_]=mySDP.Solve(sigma, tau, tol_low, nsteps)
 end_admm = timing.clock()
-print "Time used by ADMM: " + str(end_admm - start_admm) + " s"
+# Time to solve the SDP using ADMM (already given Copt, Aeq, and beq)
+print "Solve time for ADMM: " + str(end_admm - start_solve) + " s"
+# Total time (includes solving using ADMM and building the Copt, Aeq, and beq matrices)
+print "Total time for ADMM: " + str(end_admm - start_admm) + " s"
+print ""
+################################# Solve using CVX #################################
+
+# Suppress ComplexWarning so that output is not messed up
+warnings.filterwarnings("ignore")
+Xcvx,cvxtime = mlab.alignment(movie,1,nout=2)
+cvxtime = np.round(cvxtime, 6)
+# Only solving
+print "Solve time for CVX : " + str(cvxtime[0,4]) + " s"
+# Total time (includes model building, presolving, and solving)
+print "Total time for CVX : " + str(sum(cvxtime[0,2:5])) + " s"
+#print X
+#print Xcvx
